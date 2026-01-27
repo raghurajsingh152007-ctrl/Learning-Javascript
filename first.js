854b720adca5b6686fc2746c41a16bd4fbe0a703
Raghuraj Singh <raghurajsingh152007@gmail.com>
2026-01-27 22:38:47 +0530

    today I learned variables in js

--- first.js
+++ first.js
@@ -1,1 +1,17 @@
-console.log("Raghuraj Singh");
+console.log("Raghuraj Singh"); // to print somithing 
+console.log(" I am learning JS");
+age=24 
+console.log(age);// variable declarlation
+ let name="tony stark";// can be updated
+ const PI=3.14; // can not be updated
+console.log(name);
+
+// object understanding
+const student ={
+    fullName:"RaghurajSingh", // key : "value" pair
+    age:"19",
+    marks:8.5, 
+    isPass:true
+};
+student["age"] = student["age"] + 1; // vaule changing in object 
+console.log(student.age);
